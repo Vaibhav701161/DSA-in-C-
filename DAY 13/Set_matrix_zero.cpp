e438464341ee338b408777c74f7f64e7cedf6e61
Vaibhav701161 <vaibhavmittal929@gmail.com>
2025-04-02 23:46:31 +0530

    added optimized approach

--- DAY 13/Set_matrix_zero.cpp
+++ DAY 13/Set_matrix_zero.cpp
@@ -94,3 +94,38 @@ for(int i=0;i<n;i++){
         }
     }
 }
+
+// time complexity is O(n*m) and space complexity is O(1)
+
+// Optimized approach
+
+class Solution {
+    public:
+        void setZeroes(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+            int m = matrix[0].size();
+            
+            vector<int> row(n, 0);
+            vector<int> col(m, 0);
+    
+            // Step 1: Mark rows and columns that need to be set to zero
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (matrix[i][j] == 0) {
+                        row[i] = 1;
+                        col[j] = 1;
+                    }
+                }
+            }
+    
+            // Step 2: Update the matrix based on marked rows and columns
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (row[i] == 1 || col[j] == 1) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    };
+    
