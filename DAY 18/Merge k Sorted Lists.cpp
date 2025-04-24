8eedb186f79534978bef5f2553feb1366fedbc36
Vaibhav701161 <vaibhavmittal929@gmail.com>
2025-04-24 23:27:31 +0530

    added file : Merge k Sorted Lists

---
+++ DAY 18/Merge k Sorted Lists.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        ListNode* mergeKLists(vector<ListNode*>& lists) {
+            auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+            priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+            for (auto l : lists) {
+                if (l) pq.push(l);
+            }
+            ListNode dummy(0), *tail = &dummy;
+            while (!pq.empty()) {
+                ListNode* node = pq.top(); pq.pop();
+                tail->next = node;
+                tail = tail->next;
+                if (node->next) pq.push(node->next);
+            }
+            return dummy.next;
+        }
+    };
+    
